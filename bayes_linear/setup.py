2c30a6d84e13c8fd74e3d50e1f509c59219a0964
AVoss84 <a.vosseler@gmx.net>
2021-01-02 22:46:48 +0100

    first commit

---
+++ bayes_linear/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bayes_linear',
+    version='1.0.0',
+    description='Bayesian inference of linear regression models for discrete and continous response variables',
+    url='https://github.com/AVoss84/bayes-linear',
+    author='Alexander Vosseler',
+    #keywords='rest restful api flask swagger openapi flask-restplus',
+    packages = find_packages()
+    #install_requires=['flask-restplus==0.9.2', 'Flask-SQLAlchemy==2.1']
+)
+
